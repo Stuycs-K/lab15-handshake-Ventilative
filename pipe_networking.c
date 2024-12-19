4739ef1739956a152aee8ee1503600f468062de0
Renxuan Pang <rpang50@stuy.edu>
2024-12-19 14:50:42 -0500

    class

--- pipe_networking.c
+++ pipe_networking.c
@@ -1,4 +1,15 @@
 #include "pipe_networking.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <signal.h>
+
 //UPSTREAM = to the server / from the client
 //DOWNSTREAM = to the client / from the server
 /*=========================
@@ -10,12 +21,21 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_setup() {
-  int from_client = 0;
+  mkfifo("serverPipe", 0666);
+  int fd = open("serverPipe", O_RDONLY);
+  int from_client;
+  while(1){
+    int bytesRead = read(fd, from_client, 4);
+    if (bytesRead > 0){
+      remove("serverPipe");
+      break;
+    }
+  }
   return from_client;
 }
 
 /*=========================
-  server_handshake 
+  server_handshake
   args: int * to_client
 
   Performs the server side pipe 3 way handshake.
@@ -24,7 +44,8 @@ int server_setup() {
   returns the file descriptor for the upstream pipe (see server setup).
   =========================*/
 int server_handshake(int *to_client) {
-  int from_client;
+  int from_client = server_setup();
+  to_client = open(from_client, O_WRONLY);
   return from_client;
 }
 
@@ -39,7 +60,12 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
+  mkfifo(68743496, 0666);
+  int fd = open("serverPipe", O_WRONLY);
+  write(68743496, fd, 4);
+  int pfd = open(68743496, O_RDONLY);
   int from_server;
+  to_server = server_handshake(from_server);
   return from_server;
 }
 
@@ -56,5 +82,3 @@ int server_connect(int from_client) {
   int to_client  = 0;
   return to_client;
 }
-
-
